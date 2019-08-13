a101bec1772a5803333ecd447a7b2caa8bbecdf4
Denis Shkabatur <denis6705@gmail.com>
2019-08-13 13:52:08 +0300

    first commit

---
+++ elpa/git-commit-20190717.29/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20190717.29" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (with-editor "20181103")) :commit "75d0810d131e2e61ae3c683797a10a2caca96073" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
